7253c6b683a0428d31d3fd8ba8ab08ca8e60f0dc
melissakya07 <melissakya99@gmail.com>
2021-05-24 01:42:57 +0800

    Adding functions to get message every 1 sec

--- src/chatbot/run.py
+++ src/chatbot/run.py
@@ -1,4 +1,10 @@
 import tinder_api_sms
+import time
+
+
+compiled_messages = {}
+latest_message = {}
+
 
 def process_messages(messages_all, my_id):
     """
@@ -20,19 +26,46 @@ def process_messages(messages_all, my_id):
             match_ids.append(first["match_id"])
             for message in messages:
                 if message['from'] != my_id:
-                    individual_messages = individual_messages + " " + str(message['message'])
+                    individual_messages += str(message['message'])
             processed_messages.append(individual_messages)
     return processed_messages, ids, match_ids
+
+
+def check_duplicate(proc, ids):
+    global compiled_messages
+    if not compiled_messages:
+        for index in range(len(proc)):
+            compiled_messages[ids[index]] = proc[index]
+
+    for i in range(len(proc)):
+        if ids[i] in compiled_messages:
+            compiled = compiled_messages[ids[i]]
+            if proc[i] != "":
+                if proc[i] not in compiled:
+                    compiled = compiled + " " + proc[i]
+                    compiled_messages[ids[i]] = compiled
+        else:
+            compiled_messages[ids[i]] = proc[i]
+
+
+def get_message_every_1_sec(my_id, matches_info):
+    global latest_message
+    while True:
+        try:
+            messages_all = []
+            for match in matches_info:
+                messages_all.append(match.get('messages'))
+            proc, ids, match_ids = process_messages(messages_all, my_id)
+            latest_message = proc
+            check_duplicate(proc, ids)
+            time.sleep(1)
+        except:
+            print("Error getting message...maybe token expired")
+            break
 
 
 if __name__ == "__main__":
     my_id = tinder_api_sms.get_self().get("_id")
     matches = tinder_api_sms.all_matches()
     matches_info = matches.get('data').get('matches')
-    messages_all = []
-    for match in matches_info:
-        messages_all.append(match.get('messages'))
-    proc, ids, match_ids = process_messages(messages_all, my_id)
-    print(proc)
-    print(ids)
-    print(match_ids)
+    get_message_every_1_sec(my_id, matches_info)
